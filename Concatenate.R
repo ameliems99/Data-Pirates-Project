58691131bf3e8868ac83e66298346fb9d5235dae
Amelie <16agms@queensu.ca>
2022-03-22 09:37:49 -0400

    .

--- Concatenate.R
+++ Concatenate.R
@@ -1,7 +1,10 @@
-Listed <- list.files("./Scer_SNP_alignment")
-Keep <- grep("^\\w+_.*.fasta$", Listed)
-KeepList <- Listed[Keep]
+Listed <- list.files("./Scer_SNP_alignment")  #lists all the files in this folder
+Keep <- grep("^\\w+_.*.fasta$", Listed)  #vector of the files we want to concatenate
+KeepList <- Listed[Keep]  #subset the list of files we want
 
+
+
+#load all of the necessary files
 FastaList <- list()
 library(seqinr)
 setwd("./Scer_SNP_alignment")
@@ -10,6 +13,10 @@ for (i in 1:length(KeepList)) {
   FastaList[[i]] <- read.fasta(KeepList[i], as.string = TRUE, forceDNAtolower = FALSE)
 }
 
+#this loads all of the files into a List of lists (each list item is a list in itself of the sequences for each strain)
 
 
-setwd("../")
+
+setwd("../") #reset working dir to Data-Pirates-Project
+
+
