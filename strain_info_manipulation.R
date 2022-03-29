b29baf25f91ff75b135e51b53b70ccf5d76c4325
Amelie <16agms@queensu.ca>
2022-03-29 09:03:17 -0400

    If you code the import this way, it should be able to run on everyone's system as long as they have downloaded everything directly from github

---
+++ strain_info_manipulation.R
@@ -0,0 +1,32 @@
+# The purpose of this code is to output a nice csv file with 
+#the name of strain, their position in the concatenated sequence.csv
+
+#-------------------------------------------------------------------------------
+
+
+
+library(seqinr) # allows to access read.fasta // you probably have to install it first
+
+
+# we read a short csv file and we get the names of the strains for which we have SNPs 
+untar("./SNP-alignment-largeDataset+Spar.tgz") #this is the file from Github (everyone should have in project folder)
+Fastfile = read.fasta("./SNP-alignment-largeDataset+Spar/chrI_SNP_scere_largeDataset+Spar.fasta")  #this is the output of untar
+all_strains = names(Fastfile)
+
+
+# We get load the file containing the info about SNPs
+Data = read.csv("./Strain_info.csv")
+
+# we create a new data frame
+Data2 = data.frame()
+
+
+for (i in 1:length(all_strains)) { # for every strain in the SNPs files...
+  Data2[i, 1:8] <- Data[Data$Strain == all_strains[i], ] #we add a row t Data2 containing the corresponding info for the strain
+}
+
+
+# We output the data in the following file:
+write.csv(Data2, "ordered_strain_info.csv", row.names = F)
+
+
